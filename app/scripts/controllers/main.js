cfb98f1bddaf46ba1ad0f713ecd743f765f57636
gauravjain-29 <gauravjain29@hotmail.com>
2017-05-04 12:04:06 +0530

    UI Change

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,17 @@
  * Controller of the stockApp
  */
 
- angular.module('stockApp').run(function($rootScope, $location, $state, LoginService) {
-    $rootScope.$on('$stateChangeStart', 
-      function(event, toState, toParams, fromState, fromParams){ 
-          console.log('Changed state to: ' + toState);
-          console.log(toState);
-      });
-    
-      if(!LoginService.isAuthenticated()) {
+angular.module('stockApp').run(function($rootScope, $location, $state, LoginService) {
+    $rootScope.$on('$stateChangeStart',
+        function(event, toState, toParams, fromState, fromParams) {
+            console.log('Changed state to: ' + toState);
+            console.log(toState);
+        });
+
+    if (!LoginService.isAuthenticated()) {
         $state.transitionTo('login');
-      }
-  });
+    }
+});
 
 angular.module('stockApp')
     .controller('MainCtrl', function($scope, $http) {
@@ -57,6 +57,7 @@ angular.module('stockApp')
         }
 
         $scope.blockUICall();
+        $scope.isCollapsed = true;
         $http({
             method: 'GET',
             url: 'https://django-backend.herokuapp.com/stocks/'
@@ -71,7 +72,7 @@ angular.module('stockApp')
             // called asynchronously if an error occurs
             // or server returns response with an error status.
         });
-  
+
 
 
         this.awesomeThings = [
@@ -81,50 +82,66 @@ angular.module('stockApp')
         ];
 
         $scope.scripCode = '';
-        $scope.getData = function() {
-            $scope.history = true;
-            $scope.ticker = false;
-            $scope.data = '';
-            $scope.errorCode = '';
-            $scope.errorMessage = '';
-            $scope.blockUICall();
+        // $scope.getData = function(shareData) {
+        //     $scope.history = false;
+        //     $scope.ticker = true;
+        //     $scope.data = '';
+        //     $scope.errorCode = '';
+        //     $scope.errorMessage = '';
+        //     //$scope.blockUICall();
 
-            var startDateParameter = $scope.startDate ? '&start_date=' + $scope.startDate : '';
-            var endDateParameter = $scope.endDate ? '&end_date=' + $scope.endDate : '';
-            $.ajax({
-                url: ('https://www.quandl.com/api/v3/datasets/NSE/' + $scope.scripCode + '.json?api_key=-Q-zpXxS1SEg8TJQEi1L' + startDateParameter + endDateParameter),
-                dataType: 'json',
-                type: 'get',
-                success: function(response) {
-                    $scope.data = response;
-                    $scope.unblockUICall();
-                    $scope.draw();
-                    $scope.$digest();
-                },
-                error: function(response) {
-                    console.log(response);
-                    $scope.errorCode = response.status;
-                    if (response.status == 404) {
-                        $scope.errorMessage = 'Invalid Scrip Code';
-                    }
-                    $scope.unblockUICall();
-                    $scope.$digest();
-                }
-            });
-        }
+        //     var currentDate = new Date();
+        //     var day = currentDate.getDate();
+        //     var month = currentDate.getMonth() + 1;
+        //     var year = currentDate.getFullYear();
 
-        $scope.draw = function() {
+        //     var todaysDate = year+'-'+month+'-'+day;
+        //     var previousYearDate = (year-1)+'-'+month+'-'+day;
+        //     if(!$scope.startDate && !$scope.endDate)
+        //     {
+        //         $scope.startDate = previousYearDate;
+        //         $scope.endDate = todaysDate;
+        //     }
+
+        //     $scope.draw(shareData);
+
+        //     // var startDateParameter = $scope.startDate ? '&start_date=' + $scope.startDate : '';
+        //     // var endDateParameter = $scope.endDate ? '&end_date=' + $scope.endDate : '';
+        //     // $.ajax({
+        //     //     url: ('https://www.quandl.com/api/v3/datasets/NSE/' + $scope.scripCode + '.json?api_key=-Q-zpXxS1SEg8TJQEi1L' + startDateParameter + endDateParameter),
+        //     //     dataType: 'json',
+        //     //     type: 'get',
+        //     //     success: function(response) {
+        //     //         console.log(response);
+        //     //         $scope.data = response;
+        //     //         $scope.unblockUICall();
+        //     //         $scope.draw();
+        //     //         $scope.$digest();
+        //     //     },
+        //     //     error: function(response) {
+        //     //         console.log(response);
+        //     //         $scope.errorCode = response.status;
+        //     //         if (response.status == 404) {
+        //     //             $scope.errorMessage = 'Invalid Scrip Code';
+        //     //         }
+        //     //         $scope.unblockUICall();
+        //     //         $scope.$digest();
+        //     //     }
+        //     // });
+        // }
+
+        $scope.draw = function(shareData) {
             var formattedData = [];
-            var shareData = $scope.data.dataset.data;
+            //var shareData = $scope.data.dataset.data;
             for (var item in shareData) {
                 var itemData = shareData[item];
-                var itemObj = { x: new Date(itemData[0]), y: [itemData[1], itemData[2], itemData[3], itemData[5]] };
+                var itemObj = { x: new Date(itemData.Date), y: [itemData.Open, itemData.High, itemData.Low, itemData.Close] };
                 formattedData.push(itemObj);
             }
 
             var chart = new CanvasJS.Chart("chartContainer", {
                 title: {
-                    text: $scope.data.dataset.name + " Candlestick Chart",
+                    text: "Candlestick Chart",
                 },
                 exportEnabled: true,
                 axisY: {
@@ -145,6 +162,25 @@ angular.module('stockApp')
         $scope.getTicker = function() {
             $scope.history = false;
             $scope.ticker = true;
+            //$scope.data = '';
+            $scope.errorCode = '';
+            $scope.errorMessage = '';
+            $scope.dismissSearch = true;
+            //$scope.blockUICall();
+
+            var currentDate = new Date();
+            var day = currentDate.getDate();
+            var month = currentDate.getMonth() + 1;
+            var year = currentDate.getFullYear();
+
+            var todaysDate = year+'-'+month+'-'+day;
+            var previousYearDate = (year-1)+'-'+month+'-'+day;
+            if(!$scope.startDate && !$scope.endDate)
+            {
+                $scope.startDate = previousYearDate;
+                $scope.endDate = todaysDate;
+            }
+
             $scope.blockUICall();
             var postdata = {}
             postdata.ticker = $scope.scripCode;
@@ -160,15 +196,17 @@ angular.module('stockApp')
                 type: 'post',
                 data: postdata,
                 success: function(response) {
-                    $scope.tickerData = response;
+                    console.log(response);
+                    $scope.draw(response);
+                    $scope.tickerData = response.reverse();
                     $scope.$digest();
                 },
                 error: function(response) {
                     console.log(response);
                     $scope.errorCode = response.status;
-                    if (response.status == 404) {
-                        $scope.errorMessage = 'Invalid Scrip Code';
-                    }
+                    //if (response.status == 404) {
+                        $scope.errorMessage = response.responseText;
+                    //}
                     $scope.$digest();
                     $scope.unblockUICall();
                 }
@@ -189,36 +227,34 @@ angular.module('stockApp')
                 error: function(response) {
                     console.log(response);
                     $scope.errorCode = response.status;
-                    if (response.status == 404) {
-                        $scope.errorMessage = 'Invalid Scrip Code';
-                    }
+                    //if (response.status == 404) {
+                        $scope.errorMessage = response.responseText;
+                    //}
                     $scope.$digest();
                     $scope.unblockUICall();
                 }
             });
         }
 
-        $scope.setScrip = function(scripSelected)
-        {
+        $scope.setScrip = function(scripSelected) {
             $scope.scripCode = scripSelected;
             $scope.dismissSearch = true;
         }
 
-        
+
 
     });
 
 angular.module('stockApp').directive('datePicker', function() {
     var link = function(scope, element, attrs) {
         var modelName = attrs['datePicker'];
-        $(element).datepicker(
-            {
-                dateFormat: 'yy-mm-dd',
-                onSelect: function(dateText) {
-                    scope[modelName] = dateText;
-                    scope.$apply();
-                }
-            });
+        $(element).datepicker({
+            dateFormat: 'yy-mm-dd',
+            onSelect: function(dateText) {
+                scope[modelName] = dateText;
+                scope.$apply();
+            }
+        });
     };
     return {
         require: 'ngModel',
@@ -228,34 +264,33 @@ angular.module('stockApp').directive('datePicker', function() {
 });
 
 angular.module('stockApp').controller('LoginController', function($scope, $rootScope, $stateParams, $state, LoginService) {
-    $rootScope.title = "AngularJS Login Sample";
-    
-    $scope.formSubmit = function() {
-      if(LoginService.login($scope.username, $scope.password)) {
-        $scope.error = '';
-        $scope.username = '';
-        $scope.password = '';
-        $state.transitionTo('home');
-      } else {
-        $scope.error = "Incorrect username/password !";
-      }   
-    };
-    
-  });
 
-  angular.module('stockApp').factory('LoginService', function() {
+    $scope.formSubmit = function() {
+        if (LoginService.login($scope.username, $scope.password)) {
+            $scope.error = '';
+            $scope.username = '';
+            $scope.password = '';
+            $state.transitionTo('home');
+        } else {
+            $scope.error = "Incorrect username/password !";
+        }
+    };
+
+});
+
+angular.module('stockApp').factory('LoginService', function() {
     var admin = 'admin';
     var pass = 'admin123';
     var isAuthenticated = false;
-    
+
     return {
-      login : function(username, password) {
-        isAuthenticated = username === admin && password === pass;
-        return isAuthenticated;
-      },
-      isAuthenticated : function() {
-        return isAuthenticated;
-      }
+        login: function(username, password) {
+            isAuthenticated = username === admin && password === pass;
+            return isAuthenticated;
+        },
+        isAuthenticated: function() {
+            return isAuthenticated;
+        }
     };
-    
-  });
+
+});
