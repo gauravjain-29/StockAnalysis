3a69f6ea78249413448dd1c845d22973760a93a3
gauravjain-29 <gauravjain29@hotmail.com>
2017-06-27 18:56:57 +0530

    UI Enhancements

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,8 +3,6 @@ angular.module('stockApp').controller('LoginController', function($scope, $rootS
     $scope.emailCheckStatus = -1;
     $scope.checkEmail = function()
     {
-        console.log('Enetered Here');
-        
         if($scope.reg_email != null && $scope.reg_email != undefined && $scope.reg_email.includes('@'))
         {
             $scope.emailCheckStatus = 0;
@@ -52,30 +50,31 @@ angular.module('stockApp').controller('LoginController', function($scope, $rootS
     });
 
     var baseURL = 'https://django-qa.herokuapp.com/';
-    $scope.blockUICall = function(message) {
-        $.blockUI({
-            message: message,
-            css: {
-                border: 'none',
-                padding: '15px',
-                backgroundColor: '#000',
-                '-webkit-border-radius': '10px',
-                '-moz-border-radius': '10px',
-                opacity: .5,
-                color: '#fff'
+    // $scope.blockUICall = function(message) {
+    //     $.blockUI({
+    //         message: message,
+    //         css: {
+    //             border: 'none',
+    //             padding: '15px',
+    //             backgroundColor: '#000',
+    //             '-webkit-border-radius': '10px',
+    //             '-moz-border-radius': '10px',
+    //             opacity: .5,
+    //             color: '#fff'
 
-            }
-        });
-    }
+    //         }
+    //     });
+    // }
 
-    $scope.unblockUICall = function() {
-        $.unblockUI();
-    }
+    // $scope.unblockUICall = function() {
+    //     $.unblockUI();
+    // }
 
-
+    $scope.showProgressBar = false;
     $scope.login = function() {
         $scope.error = '';
-        $scope.blockUICall('Validating Credentials');
+        $scope.showProgressBar = true;
+        //$scope.blockUICall('Validating Credentials');
         $http({
             method: 'POST',
             url: baseURL + 'auth/login/',
@@ -85,14 +84,16 @@ angular.module('stockApp').controller('LoginController', function($scope, $rootS
             $scope.jwt = response.data.token;
             $cookies.put('jwtOAuthToken', $scope.jwt);
             //store.set('token', response.token);
-            $scope.unblockUICall();
+            //$scope.unblockUICall();
             var stateBeforeLogin = $cookies.get('stateBeforeLogin')
             if(!stateBeforeLogin)
                 stateBeforeLogin = 'home';
 
+            $scope.showProgressBar = false;
             $state.go(stateBeforeLogin);
         }, function errorCallback(response) {
             $scope.logged = false;
+            $scope.showProgressBar = false;
             //$scope.error = "Incorrect username/password !";
             $.notify({
                 icon: 'ti-lock',
@@ -106,14 +107,15 @@ angular.module('stockApp').controller('LoginController', function($scope, $rootS
                     align: 'center'
                 }
             });
-            $scope.unblockUICall();
+            //$scope.unblockUICall();
             // called asynchronously if an error occurs
             // or server returns response with an error status.
         });
     }
 
     $scope.register = function() {
-        $scope.blockUICall('Registration in progress.');
+        //$scope.blockUICall('Registration in progress.');
+        $scope.showProgressBar = true;
         $http({
             method: 'POST',
             url: baseURL + 'register/',
@@ -132,7 +134,8 @@ angular.module('stockApp').controller('LoginController', function($scope, $rootS
                     align: 'center'
                 }
             });
-            $scope.unblockUICall();
+            //$scope.unblockUICall();
+            $scope.showProgressBar = false;
         }, function errorCallback(response) {
             //$scope.reg_message = 'Username already taken.';
             $.notify({
@@ -147,7 +150,8 @@ angular.module('stockApp').controller('LoginController', function($scope, $rootS
                     align: 'center'
                 }
             });
-            $scope.unblockUICall();
+            $scope.showProgressBar = false;
+            //$scope.unblockUICall();
             // called asynchronously if an error occurs
             // or server returns response with an error status.
         });
